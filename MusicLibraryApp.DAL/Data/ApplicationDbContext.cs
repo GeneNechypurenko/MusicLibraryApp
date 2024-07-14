f81b52e674de4bdc521f65e9a07f77c214c8f0d7
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-07-14 18:58:09 +0300

    seed tunes

--- MusicLibraryApp.DAL/Data/ApplicationDbContext.cs
+++ MusicLibraryApp.DAL/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace MusicLibraryApp.DAL.Data
 		public DbSet<User> Users { get; set; }
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<Tune> Tunes { get; set; }
+
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) => Database.EnsureCreated();
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -25,61 +26,148 @@ namespace MusicLibraryApp.DAL.Data
 			});
 
 			string defaultPosterUrl = "res/Category/Posters/default_poster.jpg";
+			string defaultTunePosterUrl = "res/Tunes/Posters/default_poster.jpg";
+			string defaultTuneFileUrl = "res/Tunes/Upload/default_tune.mp3";
 
-			modelBuilder.Entity<Category>().HasData(new Category
+			modelBuilder.Entity<Category>().HasData(new Category { Id = 1, Genre = "Electronic", PosterUrl = defaultPosterUrl });
+			modelBuilder.Entity<Category>().HasData(new Category { Id = 2, Genre = "Hip-Hop", PosterUrl = defaultPosterUrl });
+			modelBuilder.Entity<Category>().HasData(new Category { Id = 3, Genre = "Rock", PosterUrl = defaultPosterUrl });
+			modelBuilder.Entity<Category>().HasData(new Category { Id = 4, Genre = "Classical", PosterUrl = defaultPosterUrl });
+			modelBuilder.Entity<Category>().HasData(new Category { Id = 5, Genre = "Pop", PosterUrl = defaultPosterUrl });
+			modelBuilder.Entity<Category>().HasData(new Category { Id = 6, Genre = "Jazz", PosterUrl = defaultPosterUrl });
+			modelBuilder.Entity<Category>().HasData(new Category { Id = 7, Genre = "Folk", PosterUrl = defaultPosterUrl });
+			modelBuilder.Entity<Category>().HasData(new Category { Id = 8, Genre = "Cinematic", PosterUrl = defaultPosterUrl });
+			modelBuilder.Entity<Category>().HasData(new Category { Id = 9, Genre = "Reggae", PosterUrl = defaultPosterUrl });
+
+			var tunes = new List<Tune>();
+			for (int i = 0; i < 10; i++)
 			{
-				Id = 1,
-				Genre = "Electronic",
-				PosterUrl = defaultPosterUrl
-			});
-			modelBuilder.Entity<Category>().HasData(new Category
+				tunes.Add(new Tune
+				{
+					Id = i + 1,
+					Performer = "Test",
+					Title = $"Sandwitch Blues {i + 1}",
+					FileUrl = defaultTuneFileUrl,
+					PosterUrl = defaultTunePosterUrl,
+					IsAuthorized = true,
+					IsBlocked = false,
+					CategoryId = 1
+				});
+			}
+			for (int i = 0; i < 10; i++)
 			{
-				Id = 2,
-				Genre = "Hip-Hop",
-				PosterUrl = defaultPosterUrl
-			});
-			modelBuilder.Entity<Category>().HasData(new Category
+				tunes.Add(new Tune
+				{
+					Id = i + 11,
+					Performer = "Test",
+					Title = $"Sandwitch Blues {i + 1}",
+					FileUrl = defaultTuneFileUrl,
+					PosterUrl = defaultTunePosterUrl,
+					IsAuthorized = true,
+					IsBlocked = false,
+					CategoryId = 2
+				});
+			}
+			for (int i = 0; i < 10; i++)
 			{
-				Id = 3,
-				Genre = "Rock",
-				PosterUrl = defaultPosterUrl
-			});
-			modelBuilder.Entity<Category>().HasData(new Category
+				tunes.Add(new Tune
+				{
+					Id = i + 21,
+					Performer = "Test",
+					Title = $"Sandwitch Blues {i + 1}",
+					FileUrl = defaultTuneFileUrl,
+					PosterUrl = defaultTunePosterUrl,
+					IsAuthorized = true,
+					IsBlocked = false,
+					CategoryId = 3
+				});
+			}
+			for (int i = 0; i < 10; i++)
 			{
-				Id = 4,
-				Genre = "Classical",
-				PosterUrl = defaultPosterUrl
-			});
-			modelBuilder.Entity<Category>().HasData(new Category
+				tunes.Add(new Tune
+				{
+					Id = i + 31,
+					Performer = "Test",
+					Title = $"Sandwitch Blues {i + 1}",
+					FileUrl = defaultTuneFileUrl,
+					PosterUrl = defaultTunePosterUrl,
+					IsAuthorized = true,
+					IsBlocked = false,
+					CategoryId = 4
+				});
+			}
+			for (int i = 0; i < 10; i++)
 			{
-				Id = 5,
-				Genre = "Pop",
-				PosterUrl = defaultPosterUrl
-			});
-			modelBuilder.Entity<Category>().HasData(new Category
+				tunes.Add(new Tune
+				{
+					Id = i + 41,
+					Performer = "Test",
+					Title = $"Sandwitch Blues {i + 1}",
+					FileUrl = defaultTuneFileUrl,
+					PosterUrl = defaultTunePosterUrl,
+					IsAuthorized = true,
+					IsBlocked = false,
+					CategoryId = 5
+				});
+			}
+			for (int i = 0; i < 10; i++)
 			{
-				Id = 6,
-				Genre = "Jazz",
-				PosterUrl = defaultPosterUrl
-			});
-			modelBuilder.Entity<Category>().HasData(new Category
+				tunes.Add(new Tune
+				{
+					Id = i + 51,
+					Performer = "Test",
+					Title = $"Sandwitch Blues {i + 1}",
+					FileUrl = defaultTuneFileUrl,
+					PosterUrl = defaultTunePosterUrl,
+					IsAuthorized = true,
+					IsBlocked = false,
+					CategoryId = 6
+				});
+			}
+			for (int i = 0; i < 10; i++)
 			{
-				Id = 7,
-				Genre = "Folk",
-				PosterUrl = defaultPosterUrl
-			});
-			modelBuilder.Entity<Category>().HasData(new Category
+				tunes.Add(new Tune
+				{
+					Id = i + 61,
+					Performer = "Test",
+					Title = $"Sandwitch Blues {i + 1}",
+					FileUrl = defaultTuneFileUrl,
+					PosterUrl = defaultTunePosterUrl,
+					IsAuthorized = true,
+					IsBlocked = false,
+					CategoryId = 7
+				});
+			}
+			for (int i = 0; i < 10; i++)
 			{
-				Id = 8,
-				Genre = "Cinematic",
-				PosterUrl = defaultPosterUrl
-			});
-			modelBuilder.Entity<Category>().HasData(new Category
+				tunes.Add(new Tune
+				{
+					Id = i + 71,
+					Performer = "Test",
+					Title = $"Sandwitch Blues {i + 1}",
+					FileUrl = defaultTuneFileUrl,
+					PosterUrl = defaultTunePosterUrl,
+					IsAuthorized = true,
+					IsBlocked = false,
+					CategoryId = 8
+				});
+			}
+			for (int i = 0; i < 10; i++)
 			{
-				Id = 9,
-				Genre = "Reggae",
-				PosterUrl = defaultPosterUrl
-			});
+				tunes.Add(new Tune
+				{
+					Id = i + 81,
+					Performer = "Test",
+					Title = $"Sandwitch Blues {i + 1}",
+					FileUrl = defaultTuneFileUrl,
+					PosterUrl = defaultTunePosterUrl,
+					IsAuthorized = true,
+					IsBlocked = false,
+					CategoryId = 9
+				});
+			}
+
+			modelBuilder.Entity<Tune>().HasData(tunes);
 		}
 	}
 }
---
+++ MusicLibraryApp/Models/HomePage/TunesPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicLibraryApp.Models.HomePage
+{
+	public class TunesPageViewModel : HomePageViewModel
+	{
+		public IEnumerable<TuneViewModel> Tunes { get; set; }
+		public int SelectedGenreId { get; set; }
+	}
+}
