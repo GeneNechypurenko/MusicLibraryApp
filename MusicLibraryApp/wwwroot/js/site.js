5886b663d3a6c04bc7e918c29659704654007b27
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2024-07-28 15:35:29 +0300

    complete localization

--- MusicLibraryApp/wwwroot/js/site.js
+++ MusicLibraryApp/wwwroot/js/site.js
@@ -37,3 +37,15 @@ $(document).ready(function () {
         $(this).closest('.modal').modal('hide');
     });
 });
+
+function updateTime() {
+    const timeDisplay = document.getElementById('time-display');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    timeDisplay.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
